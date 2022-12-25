62c3698a378d2c031e5c284236083776517ac25c
kkukreja12 <121345893+kkukreja12@users.noreply.github.com>
2022-12-25 22:04:27 +0530

    created area of circle

---
+++ area of circle.py
@@ -0,0 +1,7 @@
+import math as M
+Radius=float(input("Please enter the radius of a circle: ")
+area_of_circle=M*pi*Radius*Radius
+print("The Area of Circle is: ",area_of_circle)
+OUTPUT:-
+Please enter the radius of a circle:1.1
+The Area of Circle is:3.8013271108436504
